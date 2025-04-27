c80574de5106bbcb284f580603fc6173a5b7c174
Mohamed <mohmdadl232@gmail.com>
2025-04-27 19:38:29 +0300

    POM edit

--- src/main/java/Pages/P01_LoginPage.java
+++ src/main/java/Pages/P01_LoginPage.java
@@ -4,6 +4,7 @@ import Utilities.BrowserUtils;
 import Utilities.ElementUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 public class P01_LoginPage {
     private final By usernameFieldLocator = By.cssSelector("[formcontrolname=\"userName\"]");
@@ -16,19 +17,29 @@ public class P01_LoginPage {
         this.driver = driver;
     }
 
-    public void navigateToLoginPage(String url) {
+    public P01_LoginPage navigateToLoginPage(String url) {
         BrowserUtils.navigateToUrl(driver, url);
+        return this;
     }
 
-    public void enterUsername(String username) {
+    public P01_LoginPage enterUsername(String username) {
         ElementUtils.sendDataToElement(driver, usernameFieldLocator, username);
+        return this;
     }
 
-    public void enterPassword(String password) {
+    public P01_LoginPage enterPassword(String password) {
         ElementUtils.sendDataToElement(driver, passwordFieldLocator, password);
+        return this;
     }
 
-    public void clickLoginButton() {
+    public P01_LoginPage clickLoginButton() {
         ElementUtils.clickOnElement(driver, loginButtonLocator);
+        return this;
+    }
+
+    public void assertLoginWithValidCredentials(){
+        String actualUrl = driver.getCurrentUrl();
+        String expectedUrl = "https://example.com/dashboard";
+        Assert.assertEquals(actualUrl, expectedUrl, "Login with valid credentials failed.");
     }
 }
--- src/test/java/Tests/TC01_LoginTest.java
+++ src/test/java/Tests/TC01_LoginTest.java
@@ -1,4 +1,32 @@
 package Tests;
 
+import Pages.P01_LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class TC01_LoginTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void loginWithValidCredentialsTC() {
+        new P01_LoginPage(driver)
+            .navigateToLoginPage("https://example.com/login")
+            .enterUsername("validUser")
+            .enterPassword("validPassword")
+            .clickLoginButton()
+            .assertLoginWithValidCredentials();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
 }
