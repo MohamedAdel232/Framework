12e0a7f0d98e061e5d26413f6806a46e43816428
Mohamed <mohmdadl232@gmail.com>
2025-04-27 19:32:55 +0300

    Element Utils

---
+++ src/main/java/Utilities/ElementUtils.java
@@ -0,0 +1,38 @@
+package Utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class ElementUtils {
+    private ElementUtils() {
+    }
+
+    public static WebElement findElement(WebDriver driver, By locator) {
+        return driver.findElement(locator);
+    }
+
+    public static List<WebElement> findElements(WebDriver driver, By locator) {
+        return driver.findElements(locator);
+    }
+
+    public static void clickOnElement(WebDriver driver, By locator) {
+        WaitUtils.waitForElementToBeClickable(driver, locator);
+        ScrollUtils.scrollToElement(driver, locator);
+        findElement(driver, locator).click();
+    }
+
+    public static void sendDataToElement(WebDriver driver, By locator, String data) {
+        WaitUtils.waitForElementToBeVisible(driver, locator);
+        ScrollUtils.scrollToElement(driver, locator);
+        findElement(driver, locator).sendKeys(data);
+    }
+
+    public static String getDataFromElement(WebDriver driver, By locator) {
+        WaitUtils.waitForElementToBeVisible(driver, locator);
+        ScrollUtils.scrollToElement(driver, locator);
+        return findElement(driver, locator).getText();
+    }
+}
