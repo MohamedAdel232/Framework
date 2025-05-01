6744764a79fde2fc8c31d1f1c4b40e064acb4be1
Mohamed <mohmdadl232@gmail.com>
2025-05-01 20:31:39 +0300

    Edit wait & run with maven

--- src/main/java/Utilities/WaitUtils.java
+++ src/main/java/Utilities/WaitUtils.java
@@ -18,14 +18,14 @@ public class WaitUtils {
     @Step("Wait for element to be present with locator: {0}")
     public WebElement waitForElementToBePresent(By locator) {
         LogsUtils.info("Waiting for element to be present with locator:", locator.toString());
-        return new WebDriverWait(driver, Duration.ofSeconds(5))
+        return new WebDriverWait(driver, Duration.ofSeconds(Integer.parseInt(PropertiesUtils.getPropertyValue("explicitWait"))))
                 .until(driver1 -> driver1.findElement(locator));
     }
 
     @Step("Wait for element to be visible with locator: {0}")
     public WebElement waitForElementToBeVisible(By locator) {
         LogsUtils.info("Waiting for element to be visible with locator:", locator.toString());
-        return new WebDriverWait(driver, Duration.ofSeconds(5))
+        return new WebDriverWait(driver, Duration.ofSeconds(Integer.parseInt(PropertiesUtils.getPropertyValue("explicitWait"))))
                 .until(driver1 -> {
                     WebElement element = waitForElementToBePresent(locator);
                     return element.isDisplayed() ? element : null;
@@ -35,7 +35,7 @@ public class WaitUtils {
     @Step("Wait for element to be clickable with locator: {0}")
     public WebElement waitForElementToBeClickable(By locator) {
         LogsUtils.info("Waiting for element to be clickable with locator:", locator.toString());
-        return new WebDriverWait(driver, Duration.ofSeconds(5))
+        return new WebDriverWait(driver, Duration.ofSeconds(Integer.parseInt(PropertiesUtils.getPropertyValue("explicitWait"))))
                 .until(driver1 -> {
                     WebElement element = waitForElementToBeVisible(locator);
                     return element.isEnabled() ? element : null;
