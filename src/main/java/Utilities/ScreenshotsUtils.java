e5524ac63a1f5f469799089fff6be7b60001b028
Mohamed <mohmdadl232@gmail.com>
2025-05-01 20:36:12 +0300

    Screenshots Utils update

--- src/main/java/Utilities/ScreenshotsUtils.java
+++ src/main/java/Utilities/ScreenshotsUtils.java
@@ -1,9 +1,7 @@
 package Utilities;
 
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -23,6 +21,31 @@ public class ScreenshotsUtils {
             AllureUtils.attacheScreenshotToAllureReport(screenshotName, destination.getPath());
         } catch (IOException e) {
             LogsUtils.error("Failed to take screenshot: " + e.getMessage());
+        }
+    }
+
+    public static void takeElementScreenshot(String screenshotName, WebElement element) {
+        try {
+            File source = element.getScreenshotAs(OutputType.FILE);
+            File destination = new File(screenshotsDirectoryPath + screenshotName + "-" + TimestampUtils.getTimestamp() + ".png");
+            FileUtils.copyFile(source, destination);
+            LogsUtils.info("Element screenshot taken: " + destination.getPath());
+            AllureUtils.attacheScreenshotToAllureReport(screenshotName, destination.getPath());
+        } catch (IOException e) {
+            LogsUtils.error("Failed to take element screenshot: " + e.getMessage());
+        }
+    }
+
+    public static void takeElementScreenshot(WebDriver driver, String screenshotName, By locator) {
+        try {
+            WebElement element = driver.findElement(locator);
+            File source = element.getScreenshotAs(OutputType.FILE);
+            File destination = new File(screenshotsDirectoryPath + screenshotName + "-" + TimestampUtils.getTimestamp() + ".png");
+            FileUtils.copyFile(source, destination);
+            LogsUtils.info("Element screenshot taken: " + destination.getPath());
+            AllureUtils.attacheScreenshotToAllureReport(screenshotName, destination.getPath());
+        } catch (IOException e) {
+            LogsUtils.error("Failed to take element screenshot: " + e.getMessage());
         }
     }
 
